533280bea5fd48d8d5f2209229eaf2e2f8aaf773
Adrien Ogor <adrien.ogor@gmail.com>
2016-08-05 13:17:06 +0200

    fix issue #1 , no forced black color

--- src/npm-autolink.js
+++ src/npm-autolink.js
@@ -12,138 +12,153 @@ var autolink = require('../');
 program.description('Npm autolinking feature');
 
 program
-    .command('list')
-    .description('List dev packages available')
-    .action(function(cmd, options) {
-        autolink.getDevPackage()
-            .then(function(packages) {
-                //console.log(packages);
-                if (!packages) {
-                    console.log(chalk.red('No packages found'));
-                }
-                var table = new Table();
+  .command('list')
+  .description('List dev packages available')
+  .action(function(cmd, options) {
+    autolink.getDevPackage()
+      .then(function(packages) {
+        //console.log(packages);
+        if (!packages) {
+          console.log(chalk.red('No packages found'));
+        }
+        var table = new Table({
+          style: {
+            head: ["bold"],
+            border: []
+          }
+        });
 
-                _.forOwn(packages, function(versions, name) {
-                    //var style = chalk.black;
-                    var versionTable = new Table({
-                        chars: {
-                            'top': '',
-                            'top-mid': '',
-                            'top-left': '',
-                            'top-right': '',
-                            'bottom': '',
-                            'bottom-mid': '',
-                            'bottom-left': '',
-                            'bottom-right': '',
-                            'left': '',
-                            'left-mid': '',
-                            'mid': '',
-                            'mid-mid': '',
-                            'right': '',
-                            'right-mid': '',
-                            'middle': ' '
-                        },
-                        style: {
-                            'padding-left': 0,
-                            'padding-right': 0
-                        }
-                    });
-                    _.forOwn(versions, function(value, version) {
-                        var obj = {};
-                        obj[version] = value.path;
-                        versionTable.push(obj);
-                    });
+        _.forOwn(packages, function(versions, name) {
+          //var style = chalk.black;
+          var versionTable = new Table({
+            chars: {
+              'top': '',
+              'top-mid': '',
+              'top-left': '',
+              'top-right': '',
+              'bottom': '',
+              'bottom-mid': '',
+              'bottom-left': '',
+              'bottom-right': '',
+              'left': '',
+              'left-mid': '',
+              'mid': '',
+              'mid-mid': '',
+              'right': '',
+              'right-mid': '',
+              'middle': ' '
+            },
+            style: {
+              'padding-left': 0,
+              'padding-right': 0,
+              head: ["blue", "bold"],
+              border: []
+            }
+          });
+          _.forOwn(versions, function(value, version) {
+            var obj = {};
+            obj[version] = value.path;
+            versionTable.push(obj);
+          });
 
-                    var obj = {};
-                    obj[name] = versionTable.toString();
-                    table.push(obj);
-                });
+          var obj = {};
+          obj[name] = versionTable.toString();
+          table.push(obj);
+        });
 
-                console.log(table.toString());
-            })
-            .catch(function(e) {
-                console.error(e);
-            });
-    });
-
-program
-    .command('matches')
-    .description('List packages that can be linked')
-    .action(function(cmd, options) {
-        autolink.getMatches()
-            .then(function(packages) {
-                if (!packages || !packages.length) {
-                    console.log(chalk.red('No matches found'));
-                }
-                var table = new Table({
-                    head: ['Name', 'Semver Range', 'Match path', 'Match version']
-                });
-                _.each(packages, function(pack) {
-                    var style = chalk.black;
-                    table.push([style(pack.name),
-                        style(pack.requiredRange),
-                        style(pack.devPath),
-                        style(pack.devVersion)
-                    ]);
-                });
-
-                console.log(table.toString());
-            }).catch(function(e) {
-                console.error(e.stack);
-            });
-    });
+        console.log(table.toString());
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  });
 
 program
-    .command('link [name]')
-    .description('Add link, id no name provided link all matches')
-    .action(function(name, options) {
-        autolink.linkModules(name)
-            .then(function(links) {
-                displayLinks(links);
-            })
-            .catch(function(e) {
-                console.error(e);
-            });
+  .command('matches')
+  .description('List packages that can be linked')
+  .action(function(cmd, options) {
+    autolink.getMatches()
+      .then(function(packages) {
+        if (!packages || !packages.length) {
+          console.log(chalk.red('No matches found'));
+        }
+        var table = new Table({
+          head: ['Name', 'Semver Range', 'Match path', 'Match version'],
+          style: {
+            head: ["bold"],
+            border: []
+          }
+        });
+        _.each(packages, function(pack) {
+          var style = chalk.reset;
+          table.push([style(pack.name),
+            style(pack.requiredRange),
+            style(pack.devPath),
+            style(pack.devVersion)
+          ]);
+        });
+
+        console.log(table.toString());
+      }).catch(function(e) {
+      console.error(e.stack);
     });
+  });
+
+program
+  .command('link [name]')
+  .description('Add link, id no name provided link all matches')
+  .action(function(name, options) {
+    autolink.linkModules(name)
+      .then(function(links) {
+        displayLinks(links);
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  });
 
 function displayLinks(links) {
-    var table = new Table({
-        head: ['link', 'target', 'version']
-    });
-    //console.log(links);
-    _.each(links, function(link) {
-        var style = chalk.black;
-        if (link.removed) {
-            style = chalk.red.strikethrough;
-        } else if (link.added) {
-            style = chalk.bold.blue;
-        }
-        table.push([style(path.basename(link.path)), style(link.target), style(link.version)]);
-    });
+  var table = new Table({
+    head: ['link', 'target', 'version'],
+    style: {
+      head: ["bold"],
+      border: []
+    }
+  });
+  //console.log(links);
+  _.each(links, function(link) {
+    var style = chalk.reset;
+    if (link.removed) {
+      style = chalk.red.strikethrough;
+    } else if (link.added) {
+      style = chalk.bold.blue;
+    }
+    table.push([style(path.basename(link.path)), style(link.target), style(link.version)]);
+  });
 
-    console.log(table.toString());
+  console.log(table.toString());
 }
 
 program
-    .command('remove [name]')
-    .description('remove link')
-    .action(function(name, options) {
-        autolink.removeLinks(name)
-            .then(function(links) {
-                displayLinks(links);
-            }).catch(function(e) {
-                console.error(e);
-            });
+  .command('remove [name]')
+  .description('remove link')
+  .action(function(name, options) {
+    autolink.removeLinks(name)
+      .then(function(links) {
+        displayLinks(links);
+      }).catch(function(e) {
+      console.error(e);
     });
+  });
 
 program.parse(process.argv);
 
 if (!program.args.length) {
-    autolink.listLinks()
-        .then(function(linksList) {
-            displayLinks(linksList);
-        })
-        .catch(function(e) {
-            console.error(e);
-        });
+  autolink.listLinks()
+    .then(function(linksList) {
+      displayLinks(linksList);
+    })
+    .catch(function(e) {
+      console.error(e);
+    });
 }
